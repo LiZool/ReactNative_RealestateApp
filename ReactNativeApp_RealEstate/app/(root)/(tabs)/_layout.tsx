8fe49c241ad35ed152ce23f5c927c7259d4c9ea9
Li Zulu <nurliyanakai.zuliskandar@gmail.com>
2025-03-06 10:07:49 +0800

    commit
    commit

--- ReactNativeApp_RealEstate/app/(root)/(tabs)/_layout.tsx
+++ ReactNativeApp_RealEstate/app/(root)/(tabs)/_layout.tsx
@@ -51,6 +51,32 @@ const TabsLayout = () => {
                     )
                 }}
              />
+
+            <Tabs.Screen 
+                name="explore"
+                options={{
+                    title: 'Explore',
+                    headerShown: false,
+                    tabBarIcon: ({ focused }) => (
+                        <TabIcon icon={icons.search} 
+                                    focused={focused} 
+                                    title="Explore" />
+                    )
+                }}
+             />
+
+            <Tabs.Screen 
+                name="profile"
+                options={{
+                    title: 'Profile',
+                    headerShown: false,
+                    tabBarIcon: ({ focused }) => (
+                        <TabIcon icon={icons.profile} 
+                                    focused={focused} 
+                                    title="Profile" />
+                    )
+                }}
+             />
         </Tabs>
     )
 }
