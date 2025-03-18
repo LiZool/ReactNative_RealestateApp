3071c871ac2e790dbfb5e39486df5701d2a4ccf3
Li Zulu <nurliyanakai.zuliskandar@gmail.com>
2025-03-18 10:11:36 +0800

    commit
    commit

--- ReactNativeApp_RealEstate/app/(root)/(tabs)/index.tsx
+++ ReactNativeApp_RealEstate/app/(root)/(tabs)/index.tsx
@@ -1,6 +1,6 @@
 // app/(root)/(tabs)/index
 
-import { Text, View, Image, TouchableOpacity } from 'react-native';
+import { Text, View, Image, TouchableOpacity, FlatList } from 'react-native';
 import { Link } from 'expo-router';
 import tw from "tailwind-react-native-classnames";
 import { GlobalProvider } from "@/context/GlobalContext";
@@ -10,61 +10,70 @@ import Search from '@/components/Search'
 import { Card, FeaturedCard } from '@/components/Cards'
 import Filter from '@/components/Filters'
 
+// ScrollView
+// FlatList (for list of items)
+
 export default function Index () {
   return (
     <GlobalProvider>
       <SafeAreaView style={{ backgroundColor: "white", flex: 1 }}>
-        <View style={{padding: 5}}>
-          <View style={{flex: 1, flexDirection: 'row', alignItems: "center", justifyContent: "space-between", marginTop: 5}}> 
-            <View style={{flex: 1, flexDirection: 'row'}}>
-              <Image source={images.avatar} style={{height: 60, width: 60}} />
-              
-              <View style={{flex: 1, flexDirection: "column", justifyContent: 'center'}}>
-                <Text style={{ fontSize: 20 }}> Good morning </Text>
-                <Text style={{ fontSize: 16 }}> Adrian </Text>
+        <FlatList 
+          data={[1, 2, 3, 4]}
+          renderItem={({item}) => <Card />}
+          ListHeaderComponent={
+            <View style={{padding: 5}}>
+              <View style={{flex: 1, flexDirection: 'row', alignItems: "center", justifyContent: "space-between", marginTop: 5}}> 
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                  <Image source={images.avatar} style={{height: 60, width: 60}} />
+                  
+                  <View style={{flex: 1, flexDirection: "column", justifyContent: 'center'}}>
+                    <Text style={{ fontSize: 20 }}> Good morning </Text>
+                    <Text style={{ fontSize: 16 }}> Adrian </Text>
+                  </View>
+                </View> 
+
+                <Image source={images.notification} style={{height: 35, width: 35}}/>
               </View>
-            </View> 
 
-            <Image source={images.notification} style={{height: 35, width: 35}}/>
-          </View>
+              <Search /> 
 
-          <Search /> 
+              <View style={{marginVertical: 7, marginLeft: 5}}>
+                <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent:'space-between'}}>
+                  <Text> Featured </Text>
+                  <TouchableOpacity>
+                    <Text style={{fontSize: 16, color: "blue", fontWeight: 'bold', marginRight: 7}}>
+                      See All
+                    </Text>
+                  </TouchableOpacity>
+                </View>
 
-          <View style={{marginVertical: 7, marginLeft: 5}}>
-            <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent:'space-between'}}>
-              <Text> Featured </Text>
-              <TouchableOpacity>
-                <Text style={{fontSize: 16, color: "blue", fontWeight: 'bold', marginRight: 7}}>
-                  See All
-                </Text>
-              </TouchableOpacity>
+                <View style={{flex: 1, flexDirection:'row', gap: 5, marginTop: 5}}>
+                  <FeaturedCard />
+                  <FeaturedCard />
+                  <FeaturedCard />
+                </View>
+              </View>
+
+              <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent:'space-between', marginBottom: 4}}>
+                <Text> Our Recommendations </Text>
+                  <TouchableOpacity>
+                    <Text style={{fontSize: 16, color: "blue", fontWeight: 'bold', marginRight: 7}}>
+                      See All
+                    </Text>
+                  </TouchableOpacity>
+              </View>
+
+              <View style={{ marginBottom: 0, paddingBottom: 0 }}>
+                <Filter />
+              </View>
+
+              <View style={{flex: 1, flexDirection: 'row', gap: 5, marginTop: 2}}>
+                <Card />
+                <Card />
+              </View>
             </View>
-
-            <View style={{flex: 1, flexDirection:'row', gap: 5, marginTop: 5}}>
-              <FeaturedCard />
-              <FeaturedCard />
-              <FeaturedCard />
-            </View>
-          </View>
-
-          <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent:'space-between'}}>
-            <Text> Our Recommendations </Text>
-              <TouchableOpacity>
-                <Text style={{fontSize: 16, color: "blue", fontWeight: 'bold', marginRight: 7}}>
-                  See All
-                </Text>
-              </TouchableOpacity>
-          </View>
-
-          <View style={{ marginBottom: 0, paddingBottom: 0 }}>
-            <Filter />
-          </View>
-
-          <View style={{flex: 1, flexDirection: 'row', gap: 5, marginTop: 2}}>
-            <Card />
-            <Card />
-          </View>
-        </View>
+          }
+        />
 
 
         {/* <View style={{ alignItems: "flex-start", marginVertical: 5 }}>
